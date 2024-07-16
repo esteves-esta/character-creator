c2c491e15a581887e6c88a12006e0294b790ed6f
Fernanda Esteves <fernanda.esteves@clear.sale>
2024-07-16 16:47:01 -0300

    my mobile solution

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -31,6 +31,18 @@ function App() {
   return (
     <main className={styles.characterEditor}>
       <div className={styles.perspectiveBar}></div>
+      <div className={styles.characterWrapper}>
+        <Character
+          body={body}
+          head={head}
+          face={face}
+          accessory={accessory}
+          skinColor={skinColor}
+          clothesColor={clothesColor}
+        />
+
+      </div>
+
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
@@ -79,17 +91,7 @@ function App() {
         </div>
       </MaxWidthWrapper>
 
-      <div className={styles.characterWrapper}>
-        <Character
-          body={body}
-          head={head}
-          face={face}
-          accessory={accessory}
-          skinColor={skinColor}
-          clothesColor={clothesColor}
-        />
 
-      </div>
     </main>
   );
 }
